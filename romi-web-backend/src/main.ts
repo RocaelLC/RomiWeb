9f6cb4c067ee0787f9b35c0c4eceaf5a27676b7a
RocaelLC <rocaellc42@gmail.com>
2025-11-19 16:38:14 -0600

    update config

--- romi-web-backend/src/main.ts
+++ romi-web-backend/src/main.ts
@@ -1,24 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Usa el adaptador ws nativo
-  app.useWebSocketAdapter(new WsAdapter(app));
-
-  // Permite solicitudes del frontend local
   app.enableCors({
-    origin: ['http://localhost:3000'],      
-    methods: ['GET','HEAD','PUT','PATCH','POST','DELETE','OPTIONS'],
-    allowedHeaders: ['Content-Type','Authorization','X-Requested-With'],
-    credentials: true,                      
+    origin: [
+      'http://localhost:3000', 
+      'https://romi-rmsch63xn-soyrocaels-projects.vercel.app',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   });
 
-  const port = process.env.PORT ? Number(process.env.PORT) : 3001;
-  await app.listen(port);
-  console.log(`HTTP  : http://localhost:${port}`);
-  console.log(`WS    : ws://localhost:${port}/chat`);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
