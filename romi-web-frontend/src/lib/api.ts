ee2125fd4ae4b41fde46fdde10f3b8b647669c08
RocaelLC <rocaellc42@gmail.com>
2025-11-19 16:27:34 -0600

    update config

--- romi-web-frontend/src/lib/api.ts
+++ romi-web-frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 ﻿import { getToken } from "./auth";
 
 export const API_BASE =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001";
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
 
 export async function apiFetch(path: string, init: RequestInit = {}) {
   const url = `${API_BASE}${path}`;
