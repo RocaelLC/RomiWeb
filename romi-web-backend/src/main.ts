3395c26f2592996fc2be461f1e714127b34865e3
RocaelLC <rocaellc42@gmail.com>
2025-11-21 11:22:10 -0600

    update main.ts

--- romi-web-backend/src/main.ts
+++ romi-web-backend/src/main.ts
@@ -1,37 +1,22 @@
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // app.enableCors(...);
-
-  app.use((req: any, res: any, next: any) => {
-    // Origin permitido: tu dominio de Vercel
-    const origin = req.headers.origin;
-
-    if (origin === 'https://romi-web.vercel.app') {
-      res.header('Access-Control-Allow-Origin', origin);
-    }
-
-    res.header('Vary', 'Origin');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Content-Type,Authorization,X-Requested-With',
-    );
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    );
-
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(204);
-    }
-
-    next();
+  app.enableCors({
+    origin: [
+      'https://romi-web.vercel.app', // FRONT en producción
+      'http://localhost:3000',       // FRONT en dev
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+    optionsSuccessStatus: 204,
   });
 
-  await app.listen(process.env.PORT || 3001);
+  await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
