cae519d62374ae0ddff28be99d7c4f739ffd5736
RocaelLC <rocaellc42@gmail.com>
2025-11-21 11:28:29 -0600

    update main.ts

--- romi-web-backend/src/main.ts
+++ romi-web-backend/src/main.ts
@@ -6,14 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'https://romi-web.vercel.app', // FRONT en producción
-      'http://localhost:3000',       // FRONT en dev
-    ],
+    origin: true, // refleja el origin que llega (romi-web.vercel.app)
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-    optionsSuccessStatus: 204,
   });
 
   await app.listen(process.env.PORT || 3000);
